5ba7a70a8fb75850f672bd1afa61f7a26a57d53d
sachinrana01 <sachin.rana01@nagarro.com>
2020-04-12 11:39:46 +0530

    Added app intro library

---
+++ app/src/main/java/com/alfanse/feedmycity/ui/intro/IntroActivity.kt
@@ -0,0 +1,23 @@
+package com.alfanse.feedmycity.ui.intro
+
+import android.os.Bundle
+import com.alfanse.feedmycity.R
+import com.github.paolorotolo.appintro.AppIntro
+import com.github.paolorotolo.appintro.AppIntroFragment
+import com.github.paolorotolo.appintro.model.SliderPage
+
+
+class IntroActivity : AppIntro() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        val sliderPage = SliderPage()
+        sliderPage.title = title
+        /*sliderPage.description = ""
+        sliderPage.imageDrawable = image
+        sliderPage.bgColor = backgroundColor*/
+        addSlide(AppIntroFragment.newInstance(sliderPage))
+    }
+}
